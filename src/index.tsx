f45ed489e0ca4e805aff20e39c63f062dd03e581
aromiko <mikoaro1212@gmail.com>
2022-12-22 16:47:08 +0800

    Add Tailwind css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
+import 'styles/index.scss';
+
+import Home from 'pages/Home';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+// import Footer from 'components/Footer';
+import { Footer, Header } from 'components';
+
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+export default App;
